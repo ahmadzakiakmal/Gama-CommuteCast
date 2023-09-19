f6eb18e5caceea11e32bedb4976342310c9e61ba
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-09-19 14:47:32 +0700

    feat: create Location class

---
+++ src/Gama-CommuteCast/Location.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gama_CommuteCast
+{
+    internal class Location
+    {
+        // Attributes
+        private int _id;
+        private double _latitude;
+        private double _longitude;
+
+        // Constructor
+        public Location(int id, double latitude, double longitude)
+        {
+            _id  = id;
+            _latitude = latitude;
+            _longitude = longitude;
+        }
+
+        // Attribute Encapsulation
+        public int Id { get { return _id; } }
+        public double Latitude 
+        { 
+            get { return _latitude;}  
+            set { _latitude = value; }
+        }
+        public double Longitude
+        {
+            get { return _longitude;}
+            set { _longitude = value; }
+        }
+
+    }
+}
