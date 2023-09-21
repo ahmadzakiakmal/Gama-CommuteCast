ae7379ff2d67d04a3aa9bcc1b548fd0eeb98821e
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-09-21 10:17:39 +0700

    Added TODOs

--- src/Gama-CommuteCast/WeatherData.cs
+++ src/Gama-CommuteCast/WeatherData.cs
@@ -8,16 +8,17 @@ namespace Gama_CommuteCast
 {
     internal class WeatherData
     {
-        private double _temperature;
-        private double _humidity;
+        private double _temperature = 0;
+        private double _humidity = 75.0;
         private int _id;
         public WeatherData(int id)
         {
-
+            //! TODO: Complete WeatherData Constructor
         }
 
         public void updateWeatherData()
         {
+            //! TODO: Complete updateWeatherData Method
             /*Read data from API*/
             /*Update attributes*/
         }
