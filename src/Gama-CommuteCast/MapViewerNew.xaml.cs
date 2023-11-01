92ffb9b06a6e04ffe927326c1a0d6f84cc22b0b6
anisabintang <anisabintangmaharani@gmail.com>
2023-11-01 18:11:44 +0700

    Menambahkan UI Map Viewer

---
+++ src/Gama-CommuteCast/MapViewerNew.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gama_CommuteCast
+{
+    /// <summary>
+    /// Interaction logic for MapViewerNew.xaml
+    /// </summary>
+    public partial class MapViewerNew : Window
+    {
+        public MapViewerNew()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSatellite_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    
+
+        private void ButtonZoomOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonZoomIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonTypeABus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonTypeBBus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
