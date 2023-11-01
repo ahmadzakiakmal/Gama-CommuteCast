fabd472e52c9070539ae8985602d4c3737a2d305
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2023-11-01 23:59:43 +0700

    Update UnitInput.xaml.cs

--- src/Gama-CommuteCast/UnitInput.xaml.cs
+++ src/Gama-CommuteCast/UnitInput.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace Gama_CommuteCast
 {
@@ -20,6 +22,8 @@ namespace Gama_CommuteCast
     /// </summary>
     public partial class UnitInput : Page
     {
+        private string _portName = "COM3";
+        private int _baudRate = 115200;
         public UnitInput()
         {
             InitializeComponent();
@@ -27,7 +31,67 @@ namespace Gama_CommuteCast
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if()
+            if (UnitComboBox.SelectedIndex >= 0)
+            {
+                _portName = UnitComboBox.SelectedItem.ToString();
+                
+            }
         }
+    }
+
+    internal class InputUnit
+    {
+        static SerialPort _serialPort;
+        static string _portName = "COM3";
+        static int _baudRate = 115200;
+        static Parity _parity = Parity.None;
+        static int _dataBits = 8;
+        static StopBits _stopBits = StopBits.One;
+        static Handshake _handshake = Handshake.None;
+        static bool _rtsEnable = true;
+        static bool _dtrEnable = true;
+        static int _readTimeout = 500;
+
+        InputUnit(string portName, int baudRate)
+        {
+            _portName = portName;
+            _baudRate = baudRate;
+        }
+
+        public void OpenPort()
+        {
+            _serialPort = new SerialPort(_portName, _baudRate, _parity, _dataBits, _stopBits);
+            _serialPort.Handshake = _handshake;
+            _serialPort.RtsEnable = _rtsEnable;
+            _serialPort.DtrEnable = _dtrEnable;
+            _serialPort.ReadTimeout = _readTimeout;
+            _serialPort.Open();
+        }
+
+        public void ClosePort()
+        {
+            _serialPort.Close();
+        }
+
+        public bool IsConnected()
+        {
+            return _serialPort.IsOpen;
+        }
+
+        public void ReadData()
+        {
+            try
+            {
+                string message = _serialPort.ReadLine();
+                Console.WriteLine(message);
+            }
+            catch (TimeoutException) { }
+        }
+
+        public void WriteData(string message)
+        {
+            _serialPort.WriteLine(message);
+        }
+
     }
 }
