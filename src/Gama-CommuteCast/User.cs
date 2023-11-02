69dbeabe2313ddff6028fa1e47c97db9aa9e0b40
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2023-11-02 07:58:06 +0700

    Added Admin inheritance user

--- src/Gama-CommuteCast/User.cs
+++ src/Gama-CommuteCast/User.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Npgsql;
 
 namespace Gama_CommuteCast
 {
@@ -43,4 +45,50 @@ namespace Gama_CommuteCast
             // TODO: Logout Method
         }
     }
+
+    class Admin : User
+    {
+        public bool IsSuperAdmin { get; set; }
+        private NpgsqlConnection conn;
+        string connString = "Host=20.231.106.166;Port=5432;Username=postgres;Password=kemong;Database=postgres";
+        string query = "";
+        NpgsqlCommand cmd;
+
+        public Admin(int id, string username, string password, bool isSuperAdmin) : base(id, username, password)
+        {
+            IsSuperAdmin = isSuperAdmin;
+        }
+        public bool Login(string username, string password)
+        {
+            // Login
+            try
+            {
+                conn.Open();
+                string query = $"select login_user('{username}', '{password}')";
+                cmd = new NpgsqlCommand(query, conn);
+                if ((int)cmd.ExecuteScalar() == 500)
+                {
+                    throw new Exception("[500] An internal server error occured.");
+                }
+                if ((int)cmd.ExecuteScalar() == 401)
+                {
+                    throw new Exception("[401] Wrong password.");
+                }
+                if ((int)cmd.ExecuteScalar() == 404)
+                {
+                    throw new Exception("[404] User doesn't exist.");
+                }
+                conn.Close();
+                MessageBox.Show("Success");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+                return false;
+            }
+        }
+    }
+
 }
