fb780f46c70516faaf7fa83b89bf3ee9d6344d9f
anisabintang <anisabintangmaharani@gmail.com>
2023-11-16 10:32:30 +0700

    Add User Control with Bing Map

---
+++ src/Gama-CommuteCast/UserControl1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gama_CommuteCast
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonTypeABus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
